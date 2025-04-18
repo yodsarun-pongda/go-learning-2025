c38bf08c8bfc499725ff16f38e7e0c410ee0ad12
Yodsarun <yodsarun.cpe30@gmail.com>
2025-04-18 17:30:01 +0700

    follow design pattern

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"goapp/first_program"
 	log "goapp/log_config"
 	"goapp/routes"
-	"goapp/service"
+	_ "goapp/service"
 	"goapp/utils"
 
 	config "goapp/config"
@@ -17,17 +18,37 @@ func main() {
 	// Hello world program
 	tutorial_1()
 
+	// ===========================
+	// Basic Array
+	log.Info("Basic ==========================")
+	names := []string{"John", "Doe", "Smith"}
+	names = append(names, "dd")
+	names = append(names, "ddd")
+	names = append(names, "Smfdffith")
+	customerListsJson, _ := json.Marshal(names)
+	log.Info(string(customerListsJson))
+
+	// Basic Map
+	mapNames := make(map[string]int) // map[key]value
+	mapNames["John"] = 1
+	mapNames["Doe"] = 2
+	mapNames["Smith"] = 3
+	log.Info(mapNames)
+	delete(mapNames, "Smith")
+	log.Info(mapNames)
+
+	log.Info("==========================")
+	// ===========================
+
 	// TODO: Connect to DB
 	// database.MysqlConfiguration()
 	// defer database.Db.Close()
 
 	// TODO goroutine
-	threadNumber := 20
-	service.PocMultiThread(threadNumber)
+	// threadNumber := 20
+	// service.PocMultiThread(threadNumber)
 
 	// TODO channel
-	
-
 	// TODO select
 	// TODO sync.Mutex
 	// TODO sync.WaitGroup
